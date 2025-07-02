3982ed608518a427d3aacdc5b05c233baefb4869
duck123acb <dan.ramanan@gmail.com>
2025-07-02 11:05:00 -0600

    kinda working now??

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 
     let texture = rl.load_texture(&thread, "assets/sprites/kris.png").unwrap();
     let position = Vector2::new(350.0, 280.0);
-    let frame_rec = Rectangle::new(
+    let mut frame_rec = Rectangle::new(
         0.0,
         0.0,
         132.0,
@@ -21,10 +21,28 @@ fn main() {
     );
 
     let mut frame_counter = 0;
+    let mut frame_index = 0;
     let frames_speed = 8;
 
     // Main game loop
     while !rl.window_should_close() {
+        /*** LOGIC ***/
+        frame_counter += 1;
+        if frame_counter >= 60 / frames_speed {
+            frame_counter = 0;
+            frame_index += 1;
+
+            if frame_index > 7 {
+                frame_rec.x = 0.0;
+                frame_index = 0;
+            }
+            else {
+                frame_rec.x += 132.0;
+            }
+        }
+
+
+        /*** DRAWING ***/
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
 
