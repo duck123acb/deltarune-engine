f8950b78b0d8839a97ceb93e85ae78d175fced4c
duck123acb <dan.ramanan@gmail.com>
2025-07-04 18:30:07 -0600

    rude

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,18 @@ int main() {
 	SetWindowState(FLAG_VSYNC_HINT);
 	SetTargetFPS(60);
 
+	InitAudioDevice();
+
+	Sound egg;
+	Music rude_buster = LoadMusicStream("assets/audio/songs/rude_buster.mp3");
+	PlayMusicStream(rude_buster);
+
 	Sprite kris(200, 200, "assets/sprites/kris.png", 128, 0, 4);
 	Animation slash(12, 6);
 
 	while (!WindowShouldClose()) {
+		UpdateMusicStream(rude_buster);
+
 		kris.Animate(slash);
 
 		BeginDrawing();
