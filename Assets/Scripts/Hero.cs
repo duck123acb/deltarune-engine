223f097b6bd583fc3fa141ec470aad5da02e7ecc
Danush Ramanan <dan.ramanan@gmail.com>
2025-07-15 08:34:52 -0600

    move kris

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float speed;
+
+    float horizontal;
+    float vertical;
+
+    #region PLAYER_INPUT
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        vertical = context.ReadValue<Vector2>().y;
+    }
+    #endregion
+
     void Start()
     {
         
@@ -11,6 +24,7 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = new Vector2(horizontal, vertical).normalized;
+        transform.position += (Vector3)(speed * Time.deltaTime * movement);
     }
 }
