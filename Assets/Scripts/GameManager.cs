a11c11f195533ba2bd50a1983ff09985decf0d2d
Danush Ramanan <dan.ramanan@gmail.com>
2025-07-17 10:01:00 -0600

    game window size 640x480

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void
+    Awake()
+    {
+        Screen.SetResolution(640, 480, false);
+    }
+}
