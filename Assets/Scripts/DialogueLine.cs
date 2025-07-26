a65fabb9a02f92b3b269e2b709110e3965f507e0
Danush Ramanan <dan.ramanan@gmail.com>
2025-07-26 15:10:25 -0600

    speakers

--- Assets/Scripts/DialogueLine.cs
+++ Assets/Scripts/DialogueLine.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 
+public enum Speaker
+{
+    Narrator,
+    Susie,
+    Ralsei,
+    Toriel,
+    Asgore
+}
+
 [System.Serializable]
 public class DialogueLine
 {
-    public SpeakerData speaker;
+    public Speaker speaker = Speaker.Narrator;
+    public SpeakerData speakerData;
     [TextArea]
     public string line;
 }
