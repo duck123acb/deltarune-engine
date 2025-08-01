7fbdca40042182ef5ab4fc329f915d4a05da436a
Danush Ramanan <dan.ramanan@gmail.com>
2025-07-31 21:59:37 -0600

    planning

--- Assets/Editor/DialogueLineDrawer.cs
+++ Assets/Editor/DialogueLineDrawer.cs
@@ -4,6 +4,8 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(DialogueLine))]
 public class DialogueLineDrawer : PropertyDrawer
 {
+    // private string lastSpeakerName = null;
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         EditorGUI.BeginProperty(position, label, property);
@@ -14,13 +16,28 @@ public class DialogueLineDrawer : PropertyDrawer
         SerializedProperty lineProp = property.FindPropertyRelative("text");
 
         float lineHeight = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-        
+
         Rect speakerRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
         Rect portraitRect = new Rect(position.x, position.y + lineHeight, position.width, EditorGUIUtility.singleLineHeight);
         Rect voiceClipRect = new Rect(position.x, position.y + lineHeight * 2, position.width, EditorGUIUtility.singleLineHeight);
         Rect lineRect = new Rect(position.x, position.y + lineHeight * 3, position.width, EditorGUIUtility.singleLineHeight * 3);
 
         EditorGUI.PropertyField(speakerRect, speakerNameProp);
+
+        /*
+        string speakerName = speakerProp.enumNames[speakerProp.enumValueIndex];
+        if (lastSpeakerName != speakerName)
+        {
+            Sprite portrait = Resources.Load<Sprite>($"Speakers/{speakerName}/portrait");
+            AudioClip clip = Resources.Load<AudioClip>($"Speakers/{speakerName}/voice");
+
+            portraitProp.objectReferenceValue = portrait;
+            voiceClipProp.objectReferenceValue = clip;
+
+            lastSpeakerName = speakerName;
+        }
+        */
+
         EditorGUI.PropertyField(portraitRect, portraitProp);
         EditorGUI.PropertyField(voiceClipRect, voiceClipProp);
 
@@ -31,7 +48,7 @@ public class DialogueLineDrawer : PropertyDrawer
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        // return EditorGUIUtility.singleLineHeight * 10;
+        // return EditorGUIUtility.singleLineHeight * 10; // debugging
         return EditorGUIUtility.singleLineHeight * 6.2f + EditorGUIUtility.standardVerticalSpacing; // this crashed unity?????? uhh if it does it again ima tweak out
     }
 }
